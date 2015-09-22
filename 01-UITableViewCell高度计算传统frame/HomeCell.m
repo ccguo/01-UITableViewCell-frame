0976b9369fceef8dfd24d951e421acb7e3e54cce
guo <389878212@qq.com>
2015-09-22 10:17:50 +0800

    height

--- 01-UITableViewCell高度计算传统frame/HomeCell.m
+++ 01-UITableViewCell高度计算传统frame/HomeCell.m
@@ -29,14 +29,16 @@
 {
     [super layoutSubviews];
     CGFloat width = [UIScreen mainScreen].bounds.size.width;
-    CGSize labelSize = [self.title sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(width-20, 500)];
+    UIFont *font = [self.content font];
+    CGSize labelSize = [self.title sizeWithFont:font constrainedToSize:CGSizeMake(width-20, 500)];
     self.content.frame = CGRectMake(10, 10, width-20, labelSize.height);
 }
 
 + (CGFloat)calulateHeightWithtTitle:(NSString*)title
 {
     CGFloat height = 20;
-    CGSize labelSize = [title sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(300, 500)];
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    CGSize labelSize = [title sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(width-20, 500)];
     height = height + labelSize.height;
     return height;
 }
